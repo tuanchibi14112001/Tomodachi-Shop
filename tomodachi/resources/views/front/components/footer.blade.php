31ef586e99b8e10390c76b1b58d7387db4d016c6
tuanchibi14112001 <tuanchibi14112001@gmail.com>
2023-01-15 17:10:15 +0700

    sua footer

--- tomodachi/resources/views/front/components/footer.blade.php
+++ tomodachi/resources/views/front/components/footer.blade.php
@@ -3,26 +3,26 @@
    <section class="grid">
 
       <div class="box">
-         <img src="images/email-icon.png" alt="">
+         <img src="front/images/email-icon.png" alt="">
          <h3>our email</h3>
          <a href="mailto:shaikhanas@gmail.com">shaikhanas@gmail.com</a>
          <a href="mailto:anasbhai@gmail.com">anasbhai@gmail.com</a>
       </div>
 
       <div class="box">
-         <img src="images/clock-icon.png" alt="">
+         <img src="front/images/clock-icon.png" alt="">
          <h3>opening hours</h3>
          <p>00:07am to 00:10pm</p>
       </div>
 
       <div class="box">
-         <img src="images/map-icon.png" alt="">
+         <img src="front/images/map-icon.png" alt="">
          <h3>our address</h3>
          <a href="#">mumbai, india - 400104</a>
       </div>
 
       <div class="box">
-         <img src="images/phone-icon.png" alt="">
+         <img src="front/images/phone-icon.png" alt="">
          <h3>our number</h3>
          <a href="tel:1234567890">123-456-7890</a>
          <a href="tel:1112223333">111-222-3333</a>
@@ -35,5 +35,5 @@
 </footer>
 
 <div class="loader">
-   <img src="front/images/loader.gif" alt="">
+   <img src="front/front/images/loader.gif" alt="">
 </div>
