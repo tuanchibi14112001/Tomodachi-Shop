67c63248751c7df60c28b55228f924c7781de56e
unknown <kimbaoksqp123>
2023-01-17 08:07:18 +0700

    CRUD category

---
+++ tomodachi/app/Admin/Controllers/CategoryController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\Category;
+use Encore\Admin\Controllers\AdminController;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Show;
+
+class CategoryController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'Category';
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new Category());
+
+        $grid->column('id', __('Id'));
+        $grid->column('name', __('Name'));
+        $grid->column('created_at', __('Created at'))->default(date('Y-m-d H:i:s'));
+        $grid->column('update_at', __('Update at'));
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(Category::findOrFail($id));
+
+        $show->field('id', __('Id'));
+        $show->field('name', __('Name'));
+        $show->field('created_at', __('Created at'));
+        $show->field('update_at', __('Update at'))  ;
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new Category());
+
+        $form->text('name', __('Name'));
+        // $form->datetime('update_at', __('Update at'))->default(date('Y-m-d H:i:s'));
+
+        return $form;
+    }
+}
--- tomodachi/app/Admin/routes.php
+++ tomodachi/app/Admin/routes.php
@@ -12,5 +12,8 @@ Route::group([
 ], function (Router $router) {
 
     $router->get('/', 'HomeController@index')->name('home');
+    $router->resource('category', CategoryController::class);
+   
+   
 
 });
