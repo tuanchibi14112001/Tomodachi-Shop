db40f36becc50be7b0c29d59271c9708b8c5047d
tuanchibi14112001 <tuanchibi14112001@gmail.com>
2023-02-04 15:25:12 +0700

    fix search not isset name

--- tomodachi/resources/views/front/shop/search.blade.php
+++ tomodachi/resources/views/front/shop/search.blade.php
@@ -4,7 +4,11 @@
     <section class="search-form">
         <form method="post" action="{{ route('search.submit') }}">
             @csrf
+            @if(isset($name))
             <input type="text" name="search_box" placeholder="search here..." value="{{ $name }}" class="box">
+            @else
+            <input type="text" name="search_box" placeholder="search here..." value="" class="box">
+            @endif
             <button type="submit" name="search_btn" class="fas fa-search"></button>
         </form>
     </section>
